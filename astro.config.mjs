bec552cf42011e1aa2d2636d486ff65235e230c9
viys <viysyu@gmail.com>
2025-12-06 13:46:26 +0800

    update workflows & astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	// site: 'https://viys.github.io',
-  	// base: 'visy',
+  	base: 'visy',
 	integrations: [
 		starlight({
 			title: 'My Docs',
