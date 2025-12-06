653e42ffa5caf4aef1bb588f5d7e52990575d135
viys <viysyu@gmail.com>
2025-12-06 14:34:07 +0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,14 +9,13 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
-			base: '/viys',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide', slug: 'viys/guides/example' },
 					],
 				},
 				{
